c2cf49a08d3f8a301e768d2f6b41f1d40ebceed4
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-08-01 20:15:02 +0200

    refactoring

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -22,34 +22,29 @@ func NewAuthHandler(authService service.AuthService) *AuthHandler {
 }
 
 func (h *AuthHandler) Register(c *gin.Context) {
-	var request AuthRequest
-	c.ShouldBindJSON(&request)
-
-	statusCode := http.StatusOK
-	response := gin.H{
-		"status":  "success",
-		"message": "User registered successfully",
-	}
+	request := bindAuthRequest(c)
 
 	err := h.authService.Register(request.Username, request.Password)
 	if err != nil {
-		response["status"] = "error"
-
 		if errors.Is(err, service.ErrUsernameTaken) {
-			statusCode = http.StatusConflict
-			response["message"] = "Username already taken"
+			c.JSON(http.StatusConflict, gin.H{"status": "error", "message": "Username already taken"})
 		} else {
-			statusCode = http.StatusInternalServerError
-			response["message"] = "Internal server error"
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Internal server error"})
 		}
+		return
 	}
 
-	c.JSON(statusCode, response)
+	c.JSON(http.StatusOK, gin.H{"status": "success", "message": "User registered successfully"})
+}
+
+func bindAuthRequest(c *gin.Context) AuthRequest {
+	var request AuthRequest
+	c.ShouldBindJSON(&request)
+	return request
 }
 
 func (h *AuthHandler) Login(c *gin.Context) {
-	var request AuthRequest
-	c.ShouldBindJSON(&request)
+	request := bindAuthRequest(c)
 
 	token, err := h.authService.Login(request.Username, request.Password)
 	if err != nil {
@@ -62,5 +57,4 @@ func (h *AuthHandler) Login(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"status": "success", "token": token})
-
 }
