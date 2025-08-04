12a6ab36c93a02949c1281fc738279359b03a895
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-08-04 18:34:27 +0200

    implement middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/NicoPolazzi/multiplayer-queue/internal/token"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(manager token.TokenManager) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth := ctx.GetHeader("Authorization")
+		if !strings.HasPrefix(auth, "Bearer ") {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "error",
+				"message": "Invalid authorization format. Use: Bearer {token}"})
+			return
+		}
+		tokenString := strings.TrimPrefix(auth, "Bearer ")
+		username, err := manager.Validate(tokenString)
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "error", "message": err.Error()})
+			return
+		}
+
+		ctx.Set("username", username)
+		ctx.Next()
+	}
+}
---
+++ internal/middleware/auth_test.go
@@ -0,0 +1,86 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/NicoPolazzi/multiplayer-queue/internal/token"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type MockTokenManager struct {
+	mock.Mock
+}
+
+func (m *MockTokenManager) Create(username string) (string, error) {
+	args := m.Called(username)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockTokenManager) Validate(tokenString string) (string, error) {
+	args := m.Called(tokenString)
+	return args.String(0), args.Error(1)
+}
+
+type AuthMiddlewareTestSuite struct {
+	suite.Suite
+	tokenManager *MockTokenManager
+	recorder     *httptest.ResponseRecorder
+	context      *gin.Context
+}
+
+func (s *AuthMiddlewareTestSuite) SetupTest() {
+	gin.SetMode(gin.TestMode)
+	s.tokenManager = new(MockTokenManager)
+	s.recorder = httptest.NewRecorder()
+	s.context, _ = gin.CreateTestContext(s.recorder)
+}
+
+func (s *AuthMiddlewareTestSuite) TestAuthMiddlewareWhenTokenIsValidSucced() {
+	validToken := "valid.token.string"
+	expectedUsername := "testuser"
+	s.tokenManager.On("Validate", validToken).Return(expectedUsername, nil)
+
+	s.context.Request = httptest.NewRequest(http.MethodGet, "/test", nil)
+	s.context.Request.Header.Set("Authorization", "Bearer "+validToken)
+
+	handler := AuthMiddleware(s.tokenManager)
+	handler(s.context)
+
+	s.tokenManager.AssertExpectations(s.T())
+	s.False(s.context.IsAborted())
+	s.Equal(expectedUsername, s.context.GetString("username"))
+}
+
+func (s *AuthMiddlewareTestSuite) TestAuthMiddlewareWhenHeaderIsMissingAbortContext() {
+	s.context.Request = httptest.NewRequest(http.MethodGet, "/test", nil)
+	handler := AuthMiddleware(s.tokenManager)
+	handler(s.context)
+	s.True(s.context.IsAborted())
+	s.Equal(http.StatusUnauthorized, s.recorder.Code)
+	s.JSONEq(`{"status": "error", "message": "Invalid authorization format. Use: Bearer {token}"}`,
+		s.recorder.Body.String())
+}
+
+func (s *AuthMiddlewareTestSuite) TestAuthMiddlewareWhenRequestIsInvalidAbortContext() {
+	invalidToken := "invalidToken"
+	s.tokenManager.On("Validate", invalidToken).Return("", token.ErrInvalidToken)
+	s.context.Request = httptest.NewRequest(http.MethodGet, "/test", nil)
+	s.context.Request.Header.Set("Authorization", "Bearer "+invalidToken)
+
+	handler := AuthMiddleware(s.tokenManager)
+	handler(s.context)
+
+	s.tokenManager.AssertExpectations(s.T())
+	s.True(s.context.IsAborted())
+	s.Equal(http.StatusUnauthorized, s.recorder.Code)
+	s.JSONEq(`{"status": "error", "message": "invalid token"}`,
+		s.recorder.Body.String())
+}
+
+func TestAuthMiddleware(t *testing.T) {
+	suite.Run(t, new(AuthMiddlewareTestSuite))
+}
